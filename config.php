5680d97afc6bc1e02879a417d348004186b83a92
Luis Felipe de Souza Bond <69223872+lfbond@users.noreply.github.com>
2022-04-26 21:19:54 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php 
+	define('INCLUDE_PATH','http://localhost:8080/Projetos_Estudos/02-Cursos_danki/02-CursoDesenvolvimentoWEBCompleto/01-Projetos/Projeto03-CriandoSiteDinamico-JavaScript-JQueryPHP/');
+
+?>
